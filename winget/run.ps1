cbab1654219a3bbe6001dfadd38b26f371950c37
Gregor Zurowski <gregor@zurowski.org>
2024-06-17 09:53:24 +0200

    Add RHash

--- winget/run.ps1
+++ winget/run.ps1
@@ -1,3 +1,4 @@
 winget install --exact --id AndreWiethoff.ExactAudioCopy
 winget install --exact --id Git.Git
 winget install --exact --id GuinpinSoft.MakeMKV
+winget install --exact --id RHash.RHash
