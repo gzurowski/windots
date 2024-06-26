feafb83db71e21e545b564c9e043348124cee641
Gregor Zurowski <gregor@zurowski.org>
2024-06-26 12:22:29 +0200

    Add Python 3.10

--- winget/run.ps1
+++ winget/run.ps1
@@ -2,4 +2,5 @@ winget install --exact --id AndreWiethoff.ExactAudioCopy
 winget install --exact --id Git.Git
 winget install --exact --id GuinpinSoft.MakeMKV
 winget install --exact --id Mp3tag.Mp3tag
+winget install --exact --id Python.Python.3.10
 winget install --exact --id RHash.RHash
